5672834bb92313cb95db74f86211a5ae974fecbf
Ryan Ariel Winata <ryanarielwinata@gmail.com>
2022-04-20 01:33:42 +0700

    git last push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -56,6 +56,7 @@ const Navbar = () => {
           {/* <Menu.Item icon={<MoneyCollectOutlined />}>
             <Link to="/exchanges">Exchanges</Link>
           </Menu.Item> */}
+
           <Menu.Item icon={<BulbOutlined />}>
             <Link to="/news">News</Link>
           </Menu.Item>
